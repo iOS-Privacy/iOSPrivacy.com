63f3d8da2147ce548fab058b5a1ca9d7a5af4170
intr0 <1548460-intr0@users.noreply.gitlab.com>
2022-06-27 07:04:17 +0000

    Update cacheClean.js

--- public/js/cacheClean.js
+++ public/js/cacheClean.js
@@ -1,7 +1,1 @@
-if ('serviceWorker' in navigator) {
-  caches.keys().then(function(cacheNames) {
-    cacheNames.forEach(function(cacheName) {
-      caches.delete(cacheName);
-    });
-  });
-}
+if('serviceWorker'in navigator){caches.keys().then(function(cacheNames){cacheNames.forEach(function(cacheName){caches.delete(cacheName)})})}
