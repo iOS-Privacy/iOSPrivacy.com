3787f20c080fb1bb74c156a798fdf743b3809874
intr0 <gitlab@antinazi.org>
2022-07-13 00:29:56 +0000

    Delete sw.js

--- public/sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",function(n){n.waitUntil(caches.open("v2").then(function(n){return n.addAll(["/","/index.html","/css/","/icons/","/img/","/js/","/touch-icon-ipad-retina.png","/touch-icon-iphone-retina.png","/apple-touch-icon.png","/favicon.png","/favicon.svg","/favicon.ico","/sw.js","/manifest.webmanifest"])}))});
