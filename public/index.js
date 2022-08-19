225fcd5190512e23f2848f9b813d1d2b3f13db9d
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-08-18 20:27:11 -0400

    Update index.js
    Signed-off-by: intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>

--- public/index.js
+++ public/index.js
@@ -1,15 +1,8 @@
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
     window
-        .addEventListener('load', function () {
+        .addEventListener("load", function () {
             navigator
                 .serviceWorker
-                .register('service-worker.js')
-                .then(function (registration) {
-                    // Registration was successful
-                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                }, function (err) {
-                    // registration failed :(
-                    console.log('ServiceWorker registration failed: ', err);
-                });
+                .register("service-worker.js");
         });
 }
