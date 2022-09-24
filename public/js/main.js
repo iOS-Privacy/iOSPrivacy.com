1169b6d0bd4198711c490b1a254992b92b373f98
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-09-24 13:17:39 -0400

    Delete main.js

--- public/js/main.js
+++
@@ -1,5 +0,0 @@
-navigator.serviceWorker.register("/service-worker.js", {
-    scope: "."
-}).then(function (registration) {
-    console.log("The service worker has been registered", registration);
-});
