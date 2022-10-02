a18e37c5a93576f00f992108c44288fd48d50b51
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-10-02 17:20:34 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,6 @@ if ("serviceWorker" in navigator) {
         .addEventListener("load", function () {
             navigator
                 .serviceWorker
-                .register("sw.js");
+                .register("/service-worker.js");
         });
 }
