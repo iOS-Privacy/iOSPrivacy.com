b2ae0614c3f7c6d5bed528db0d0b21062f01bcb4
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-10-10 21:17:25 -0400

    Update fetch.js

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,1 +1,6 @@
-if('serviceWorker'in navigator){navigator.serviceWorker.register('./sw.js',{scope:'./'}).then((reg)=>{console.log('Registration succeeded. Scope is '+reg.scope);)};
+if ('serviceWorker' in navigator) {
+    navigator
+        .serviceWorker
+        .register('./sw.js', {scope: './'})
+        .then((reg) => {
+            console.log('Registration succeeded. Scope is ' + reg.scope);)};
