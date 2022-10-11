fbf2281b3ab82f9d9d11792aa775ee99eb3b6605
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-10-10 21:16:08 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'iosp-cache';
+
+const PRECACHE_ASSETS = [
+    '/',
+    '/img/',
+    '/bimi/',
+    '/res/',
+    '/domainvoider/',
+    '/ivoid/',
+    '/urlhaus/',
+    '/privacy/',
+    '/css/',
+    '/js/'
+]
+
+self.addEventListener('install', event => {
+    event.waitUntil((async() => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async() => {
+        const cache = await caches.open(CACHE_NAME);
+
+        const cachedResponse = await cache.match(event.request);
+
+        if (cachedResponse !== undefined) {
+
+            return cachedResponse;
+        } else {
+
+            return fetch(event.request)
+        };
+    });
+});
