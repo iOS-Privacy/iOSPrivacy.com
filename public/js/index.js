1b776add88cc58a193b932b6325cd483e70eb4f6
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-10-15 14:58:00 -0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,6 @@ if ("serviceWorker" in navigator) {
             navigator
                 .serviceWorker
                 .register("/sw.js", {scope: "/"})
-                .then(reg) (=> {
-                console.log("Registration Complete"))
-            })
+                .then(reg)(=> {
+                    console.log("Registration Complete"))})
         };
