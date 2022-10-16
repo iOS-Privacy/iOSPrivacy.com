11497a26fada75328f610ff972ab29b7b712100b
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-10-16 19:33:49 -0400

    Delete fetch.js

--- public/js/fetch.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator
-        .serviceWorker
-        .register('sw.js', {scope: '/'})
-        .then((reg) => {
-            console.log('Registration succeeded. Scope is ' + reg.scope))};
