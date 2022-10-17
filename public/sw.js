8e32e664e2a506436ebcbe3994e229e46cac1168
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-10-17 15:25:29 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,14 @@
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.open(cacheName)
-            .then(function(cache) {
-                cache.match(event.request)
-                    .then( function(cacheResponse) {
-                        fetch(event.request)
-                            .then(function(networkResponse) {
-                                cache.put(event.request, networkResponse)
-                            })
-                        return cacheResponse || networkResponse
-                    })
-            })
-    )
-});
+self
+    .addEventListener("fetch", function (event) {
+        event.respondWith(caches.open(cacheName).then(function (cache) {
+            cache
+                .match(event.request)
+                .then(function (cacheResponse) {
+                    fetch(event.request)
+                        .then(function (networkResponse) {
+                            cache.put(event.request, networkResponse);
+                        });
+                    return cacheResponse || networkResponse;
+                });
+        }));
+    });
