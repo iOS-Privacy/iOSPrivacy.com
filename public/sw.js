368774554c21140625f2afdc9ffa2a90dd0d61dc
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-10-18 18:32:36 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,12 @@
-self
-    .addEventListener("fetch", function (event) {
-        event.respondWith(caches.open(cacheName).then(function (cache) {
-            cache
-                .match(event.request)
-                .then(function (cacheResponse) {
-                    fetch(event.request)
-                        .then(function (networkResponse) {
-                            cache.put(event.request, networkResponse);
-                        });
-                    return cacheResponse || networkResponse;
-                });
-        }));
-    });
+var cacheName = "iOS-1";
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(caches.open(cacheName).then(function(cache) {
+        cache.match(event.request).then(function(cacheResponse) {
+            fetch(event.request).then(function(networkResponse) {
+                cache.put(event.request, networkResponse);
+            });
+            return cacheResponse || networkResponse;
+        });
+    }));
+});
