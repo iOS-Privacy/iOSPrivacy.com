af046648584c0f650339e4fb9372925379649f38
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-11-08 11:02:29 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
-        navigator.serviceWorker.register("sw.js");
+        navigator.serviceWorker.register("appCache.js");
     });
 }
