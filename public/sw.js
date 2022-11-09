38a891a462926772b317bb8a7b9d0597c1778ec6
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-11-08 19:46:46 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,54 @@
-self.addEventListener("fetch", function(event) {
-    event.respondWith(caches.open(cacheName).then(function(cache) {
-        cache.match(event.request).then(function(cacheResponse) {
-            fetch(event.request).then(function(networkResponse) {
-                cache.put(event.request, networkResponse);
-            });
-            return cacheResponse || networkResponse;
-        });
-    }));
+var CACHE_NAME = "ios-v3";
+var urlsToCache = [
+    ".",
+    "/",
+    "/js",
+    "/css",
+    "/img",
+    "/res",
+    "/icons",
+    "/font",
+    "/domainvoider",
+    "/ivoid",
+    "/urlhaus",
+    "/privacy",
+    "./well-known"
+];
+self.addEventListener("install", (event) => {
+    caches
+        .open(CACHE_NAME)
+        .then(function (cache) {
+            Promise.all(urlsToCache.map(function (url) {
+                cache.add(url)
+            }))
+        })
+});
+self.addEventListener("fetch", (event) => {
+    event.respondWith(caches.match(event.request).then(function (response) {
+        if (response) {
+            return response
+        }
+        return fetch(event.request).then(function (response) {
+            if (!response || response.status !== 200 || response.type !== "basic") {
+                return response
+            }
+            var responseForCache = response.clone();
+            caches
+                .open(CACHE_NAME)
+                .then(function (cache) {
+                    cache.put(event.request, responseForCache)
+                });
+            return response
+        })
+    }))
+});
+self.addEventListener("activate", (event) => {
+    var cacheWhitelist = ["ios-v3"];
+    event.waitUntil(caches.keys().then((cacheNames) => {
+        return Promise.all(cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName)
+            }
+        }))
+    }))
 });
