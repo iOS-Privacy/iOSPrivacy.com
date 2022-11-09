7f5e7945c5d24e1b15bd817ee651a1138e230363
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-11-09 17:26:33 -0500

    Delete app.js

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-    window
-        .addEventListener("load", function () {
-            navigator
-                .serviceWorker
-                .register("sw.js");
-        });
-}
