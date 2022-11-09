81046ab83e8f52ca12b8c7b8a8532b4b2675a7bf
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-11-09 15:10:56 -0500

    Delete fetchFiles.js

--- public/js/fetchFiles.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-    window
-        .addEventListener("load", function () {
-            navigator
-                .serviceWorker
-                .register("appCache.min.js");
-        });
-}
